57293001af1268a416d252857afc92cc2d1961a2
CALLUM BURNS <CBU787@GMAIL.COM>
2017-03-18 15:07:35 -0700

    Created models and controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'owners/index'
+
+  get 'owners/new'
+
+  get 'owners/create'
+
+  get 'reservations/index'
+
+  get 'reservations/new'
+
+  get 'reservations/create'
+
+  get 'customers/index'
+
+  get 'customers/new'
+
+  get 'customers/create'
+
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
