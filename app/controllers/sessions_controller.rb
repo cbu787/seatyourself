e8a9e6f29a6b1a5a62d670befe2dfbc144ce9d6f
CALLUM BURNS <CBU787@GMAIL.COM>
2017-03-19 17:25:07 -0700

    Added owner/customer logic and destroy logic to Sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,32 @@ class SessionsController < ApplicationController
   end
 
   def create
-    customer = Customer.find_by(params[:email])
-    if customer && customer.authenticate(params[:password])
-      session[:customer_id] = customer_id
-      redirect_to restaurants_url
+    if params[:user_type] == "customer"
+      customer = Customer.find_by(params[:email])
+      if customer && customer.authenticate(params[:password])
+        session[:customer_id] = customer_id
+        session[:owner_id] = nil
+        redirect_to restaurants_url
+      else
+        render :new
+      end
+    elsif params[:user_type] == "owner"
+      owner = Owner.find_by(params[:email])
+      if owner && owner.authenticate(params[:password])
+        session[:owner_id] = owner_id
+        session[:customer_id] = nil
+        redirect_to restaurants_url
+      else
+        render :new
+      end
+    else
+      render :new
+    end
   end
 
   def destroy
+    session[:customer_id] = nil
+    session[:owner_id] = nil
+    redirect_to restaurants_url
   end
 end
