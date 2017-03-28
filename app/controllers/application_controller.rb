c007f3a71e36671bbfbff00199d7bde5d346faaa
CALLUM BURNS <CBU787@GMAIL.COM>
2017-03-27 20:23:23 -0700

    Updated time format.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def ensure_logged_in
     unless current_user
-      redirect_to new_sessions_path
+      redirect_to new_session_path
     end
   end
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -15,7 +15,7 @@ class ReservationsController < ApplicationController
     @reservation = @restaurant.reservations.build(reservation_params)
     @reservation.customer = current_user
     if @reservation.save
-      redirect_to customer_path(current_user)
+      redirect_to customers_path(current_user)
     else
       render new
     end
