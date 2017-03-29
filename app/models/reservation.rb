51ef5a48b2375086401942dabb332e1d46b9449a
CALLUM BURNS <CBU787@GMAIL.COM>
2017-03-28 20:20:54 -0700

    Validations for enough seats to accommodate new reservation and restaurant open hours; general validations for fields in signup and restaurant creation.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,19 @@
 class Reservation < ApplicationRecord
   belongs_to :restaurant
   belongs_to :customer
+  validates :operating_hours
+
+  def operating_hours
+    if (time < restaurant.open_time) || (time >= restaurant.close_time)
+      errors.add(:not_open, "Restaurant closed.")
+    end
+  end
+
+  def availability
+    seats_taken = Reservation.all.where(date: date, time: time).(:party_size)
+    
+    if (restaurant.capacity - seats_taken) < party_size
+      errors.add(:over_capacity, "Sorry, we cannot accommodate your party size.")
+    end
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,9 @@ class Restaurant < ApplicationRecord
   belongs_to :owner
   has_many :reservations
   has_many :customers, through: :reservations
+  validates :name, :capacity, :description, :phone_number, :open_time, :close_time, presence: true
+  validates :capacity, numericality: {only_integer: true, greater_than: 0}
+  validates :price, numericality: {only_integer: true, greater_than: 0, less_than: 6}
+  validates :phone_number, numericality: {only_integer: true}, length: {is: 10}
+  validates :email, uniqueness: true
 end
